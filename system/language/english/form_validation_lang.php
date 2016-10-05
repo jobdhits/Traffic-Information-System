7a778ac1e9543eff0ecffb13fc5ab07ce0b0691b
Job Sofaar <jobdhits@gmail.com>
2016-10-05 19:26:15 +0700

    Add files via upload

---
+++ system/language/english/form_validation_lang.php
@@ -0,0 +1,25 @@
+<?php
+
+$lang['required']			= " %s harus diisi.";
+$lang['isset']				= " %s harus memiliki sebuah nilai.";
+$lang['valid_email']		= " %s harus berisi alamat email yang valid.";
+$lang['valid_emails']		= " %s harus berisi semua alamat email yang valid.";
+$lang['valid_url']			= " %s harus berisi URL yang valid.";
+$lang['valid_ip']			= " %s harus berisi alamat IP valid.";
+$lang['min_length']			= " %s minimal harus sebanyak %s karakter.";
+$lang['max_length']			= " %s maximal harus sebanyak %s karakter.";
+$lang['exact_length']		= " %s harus tepat sebanyak %s karakter.";
+$lang['alpha']				= " %s harus berisi karakter alphabet.";
+$lang['alpha_numeric']		= " %s harus berisi karakter alpha-numeric.";
+$lang['alpha_dash']			= " %s harus berisi karakter alpha-numeric, underscores dan dashes.";
+$lang['numeric']			= " %s harus berisi bilangan.";
+$lang['is_numeric']			= " %s harus berisi karakter numerik.";
+$lang['integer']			= " %s harus berisi integer.";
+$lang['regex_match']		= " %s tidak sesuai dengan format yang disediakan.";
+$lang['matches']			= " %s tidak cocok dengan field %s .";
+$lang['is_natural']			= " %s harus berisi bilangan positif.";
+$lang['is_natural_no_zero']	= " %s harus berisi bilangan yang besar dari nol.";
+
+
+/* End of file form_validation_lang.php */
+/* Location: ./system/language/english/form_validation_lang.php */
